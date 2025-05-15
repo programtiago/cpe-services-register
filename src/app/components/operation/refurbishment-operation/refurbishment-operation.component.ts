0b5bc85067e6bf121a9a40602c3e7a2204ad4073
progr <programtiago@gmail.com>
2025-05-15 15:03:08 +0100

    Refurbishment Component: Added template directive to serialNumberInput to after validate cpe choosen focus the serialNumberInput

--- src/app/components/operation/refurbishment-operation/refurbishment-operation.component.ts
+++ src/app/components/operation/refurbishment-operation/refurbishment-operation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RefurbishmentService } from './services/refurbishment.service';
 import { Cpe } from '../../../../model/cpe';
 
@@ -12,15 +12,37 @@ export class RefurbishmentOperationComponent {
   cpesAvailable: Cpe[] = [] //represent all the cpes available from json file
   cpeChoosen: Cpe | null = null; //represents the Cpe selected from the mat-select list 
 
+  @ViewChild('serialNumberInput') serialNumberInput!: ElementRef; //template directive to identify the serial input on the DOM
+  serialNumberScanned: string = '';
+  serialNumberValid: boolean = false;
+
+  //represents the error to show after sn scanned: 
+  /* - if doesn't follow the parten of cpe choosen 
+     - if doesn't has a test
+     - if doesn't is in stock - can have status dispatched or doesn't have a reception id
+     - if has a test NOK
+  */   
+  cpeMessageErrorNotValid: string = '' 
+
   constructor(private refurbishmentService: RefurbishmentService){
     this.refurbishmentService.getAllCpes().subscribe((res) => {
       this.cpesAvailable = res;
-      console.log("CPES: ", res)
     })
   }
 
-  updateCpeChoosen(){
-    console.log("CPE CHOOSEN: ", this.cpeChoosen)
+  //focus serialNumberInput field after validate the cpe choosen
+  chooseCpeChoosen(){
+    let cpeSelected: string = ''
+
+    if (this.cpeChoosen){
+      cpeSelected = this.cpeChoosen.design; 
+
+      setTimeout(() => {
+        if (this.serialNumberInput && this.serialNumberInput.nativeElement){
+          this.serialNumberInput.nativeElement.focus();
+        }
+      }, 200);
+    }
   }
   
 }
