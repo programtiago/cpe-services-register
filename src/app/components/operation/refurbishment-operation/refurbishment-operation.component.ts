002b7b0aa8f658dbe1d5bdbfac5d133bd98b2251
progr <programtiago@gmail.com>
2025-05-15 15:47:47 +0100

    Refrubishment component: Method validateSn created and implemented to verify if the length and the prefix of serial number scanned. Shows the cpe error message if is not. Styled cpe-error-message

--- src/app/components/operation/refurbishment-operation/refurbishment-operation.component.ts
+++ src/app/components/operation/refurbishment-operation/refurbishment-operation.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RefurbishmentService } from './services/refurbishment.service';
 import { Cpe } from '../../../../model/cpe';
+import { CPE_SN_FORMATS } from '../../../../utils/serialNumberFormat';
 
 @Component({
   selector: 'app-refurbishment-operation',
@@ -13,7 +14,6 @@ export class RefurbishmentOperationComponent {
   cpeChoosen: Cpe | null = null; //represents the Cpe selected from the mat-select list 
 
   @ViewChild('serialNumberInput') serialNumberInput!: ElementRef; //template directive to identify the serial input on the DOM
-  serialNumber: string = '';
   serialNumberScanned: string = '';
   serialNumberValid: boolean = false;
 
@@ -49,9 +49,41 @@ export class RefurbishmentOperationComponent {
   handleSerialNumberScan(event: KeyboardEvent){
     if (event.key === 'Tab'){
       event.preventDefault(); //doesn't allow to change focus to the next field
-      this.serialNumber = (event.target as HTMLInputElement).value; //assign to serialNumber where happened the event
+      this.serialNumberScanned = (event.target as HTMLInputElement).value; //assign to serialNumber where happened the event
       console.log("Pressed tab key on serial field")
+      this.validateSn(this.serialNumberScanned)
     }
+  }
+
+
+  validateSn(sn: string){
+    //if the cpe wasn't choosen the sn isn't valid
+    if (!this.cpeChoosen){
+      this.serialNumberValid = false;
+      return;
+    }
+
+    const format = CPE_SN_FORMATS[this.cpeChoosen.design];
+    //if the format doesn't match the sn isn't valid
+    if (!format){
+      this.serialNumberValid = false;
+      return;
+    } 
+
+    const hasValidPrefix = format.prefix.some(prefix => sn.startsWith(prefix)) //check if the prefix matches with cpe choosen
+    const hasValidLength = format.length.includes(sn.length) //check if the length of the serial number matches with cpe choosen
+
+    this.serialNumberValid = hasValidLength && hasValidPrefix;
+
+    //if the sn is not valid selected the entire text - allow the user to scan another sn
+    if (!this.serialNumberValid){
+      const serial = <HTMLInputElement>this.serialNumberInput.nativeElement;
+      serial.select();
+      this.cpeMessageErrorNotValid = `Serial Number indicated: ${this.serialNumberScanned} doesn't match with the cpe choosen ! `;
+      return;
+    }
+
+    this.cpeMessageErrorNotValid = '';
   }
   
 }
